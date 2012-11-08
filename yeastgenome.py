58cbbab6ba659d6bf90f5ce4a13addd3cddc0174
Matthias <da-kaiser@der-ball-ist-rund.net>
2012-11-08 18:58:59 +0100

    planned

--- yeastgenome.py
+++ yeastgenome.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python
 
+#
+# Python library to access the
+# Saccharomyces Genome Database (SGD)
+# http://yeastgenome.org/
+#
+
+# Official API:
 # http://yeastmine.yeastgenome.org/yeastmine/api.do?subtab=python
+
+# get locus example:
+# http://db.yeastgenome.org/cgi-bin/locus.pl?locus=YML007w
--- yeastract.py
+++ yeastract.py
@@ -1,9 +1,37 @@
 #!/usr/bin/python
 
-def query(ORF):
+#
+# Python library to access the
+# Saccharomyces Transcription Factor Database (Yeastract)
+# http://yeastract.org/
+#
+
+def query_ORF(ORF):
 	from httpclient import HttpClient
-	
 	browser = HttpClient()
 	browser.GET('http://yeastract.com/view.php?existing=locus&orfname=YPL089c')
-	
 	return str(browser.Page)
+
+def get_promotor(ORF):
+	# http://yeastract.com/download.php?type=promoter&id=YPL089c
+	return
+
+def get_gene_sequence(ORF):
+	# http://yeastract.com/download.php?type=gene&id=YPL089c
+	return
+
+def find_documented_regulators(ORF):
+	# view-source:http://yeastract.com/findregulators.php?type=doc&genes=YPL089c
+	return
+
+def find_potential_regulators(ORF):
+	# http://yeastract.com/findregulators.php?type=pot&genes=YPL089c
+	return
+
+def find_documented_regulated_genes(TF):
+	# http://yeastract.com/findregulated.php?type=doc&tfs=Yap1p
+	return
+
+def find_potential_regulated_genes(TF):
+	# http://yeastract.com/findregulated.php?type=pot&tfs=Yap1p
+	return
