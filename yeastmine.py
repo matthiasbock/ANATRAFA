0f306178df97cc60dad4897b0c66d607b00b4cb7
Matthias <da-kaiser@der-ball-ist-rund.net>
2012-11-14 13:02:25 +0100

    yeastmine.py

---
+++ yeastmine.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+#
+# Python library to access the
+# Saccharomyces Genome Database (SGD)
+# http://yeastgenome.org/
+#
+
+# Official API:
+# http://yeastmine.yeastgenome.org/yeastmine/api.do?subtab=python
+
+from intermine import *
+
+service = Service("http://yeastmine.yeastgenome.org/yeastmine/service")
+
+# List all GO annotations for a specified gene. Searches for the
+# primaryIdentifier (SGDID), secondaryIdentifier (Systematic Name), symbol
+# (Standard Gene Name) and wild card queries (such as *YAL*) are supported. 
+# Manually curated, high-throughput, and computational GO annotations are
+# included. Genes include Uncharacterized and Verified ORFs, pseudogenes,
+# transposable element genes, RNAs, and genes Not in Systematic Sequence of
+# S228C.
+
+template = service.get_template('Gene_GO')
+
+# You can edit the constraint values below
+# A    Gene    Show GO annotations for gene:
+
+rows = template.rows(
+				A = {"op": "LOOKUP", "value": "YAL018C", "extra_value": "S. cerevisiae"}
+				)
+for row in rows:
+	print row
